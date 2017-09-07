7e4ca81753f97aa0e2975e5c50af3c28bd7d0b2b
cesar fernando bonilla rueda <cesar fernando bonilla rueda>
2017-09-06 23:06:38 -0600

    Updating build script

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
+pushd %cd%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64     
-SET mypath=%~dp0
-echo %mypath:~0,-1%
+popd
 set compilerflags=/Od /Zi /EHsc
-set linkerflags=/OUT:%mypath:~0,-1%\bin\bash.exe
-cl.exe %compilerflags% %mypath:~0,-1%\main.cpp /link %linkerflags%
+set linkerflags=/OUT:bin\bash.exe
+cl.exe %compilerflags% main.cpp /link %linkerflags%
 cd %mypath:~0,-1%
